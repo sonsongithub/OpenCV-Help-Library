fe23bf993b1f21c18729e4ef101d66cb465a4b51
sonson <yoshida.yuichi@gmail.com>
2011-05-09 01:50:54 +0900

    Added test code using RGB color scale.

--- OpenCVHelpLibrary/test.m
+++ OpenCVHelpLibrary/test.m
@@ -67,6 +67,51 @@ void testInCaseSourceIsGrayBuffer() {
 	
 }
 
+void testInCaseSourceIsRGBBuffer() {
+	
+	// original pixel data
+	int originalWidth = 32;
+	int originalHeight = 32;
+	unsigned char* original = (unsigned char*)malloc(sizeof(unsigned char) * originalWidth * originalHeight * 3);
+	
+	// make test pattern
+	for (int x = 0; x < originalWidth; x++) {
+		for (int y = 0; y < originalHeight; y++) {
+			if (y <= originalHeight / 2 && x <= originalWidth / 2) {
+				original[y * originalWidth * 3 + x * 3 + 0] = 255;
+				original[y * originalWidth * 3 + x * 3 + 1] = 0;
+				original[y * originalWidth * 3 + x * 3 + 2] = 0;
+			}
+			else if (y <= originalHeight / 2 && x > originalWidth / 2) {
+				original[y * originalWidth * 3 + x * 3 + 0] = 0;
+				original[y * originalWidth * 3 + x * 3 + 1] = 255;
+				original[y * originalWidth * 3 + x * 3 + 2] = 0;
+			}
+			else if (y > originalHeight / 2 && x <= originalWidth / 2) {
+				original[y * originalWidth * 3 + x * 3 + 0] = 0;
+				original[y * originalWidth * 3 + x * 3 + 1] = 0;
+				original[y * originalWidth * 3 + x * 3 + 2] = 255;
+			}
+			else if (y > originalHeight / 2 && x > originalWidth / 2) {
+				original[y * originalWidth * 3 + x * 3 + 0] = 255;
+				original[y * originalWidth * 3 + x * 3 + 1] = 255;
+				original[y * originalWidth * 3 + x * 3 + 2] = 255;
+			}
+		}
+	}
+	
+	// Convert to IplImage(RGB)
+	
+	// Convert to CGImageRef from IplImage
+	
+	// Convert to UIImage from IplImage
+	
+	// Convert to IplImage(RGB) from CGImageRef
+	
+	// Convert to IplImage(RGB) from UIImage
+	
+}
+
 void test() {
 	
 }
